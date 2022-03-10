e3c9425d61c08797df6d9a6d5a1a6cde8ab2d274
try <dlsduf@gmail.com>
2022-03-10 22:58:53 +0900

    [Feat] #8 최상위로 이동하는 버튼 생성
    -window.scrollTo()사용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,11 +5,13 @@ import CommentBox from '../components/comments/CommentBox';
 import styled from 'styled-components';
 import GridList from '../components/GridList';
 import Sort from '../components/main/Sort';
+import MoveToTopBtn from '../components/MoveToTopBtn';
 export default function Main() {
   return (
     <Wrapper>
       <Sort />
       <GridList />
+      <MoveToTopBtn />
     </Wrapper>
   );
 }
