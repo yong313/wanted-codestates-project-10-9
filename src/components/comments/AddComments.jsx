808e3f0ce33c7c08cd5a0972adddf5dc1d72211a
가즈아 <wjsdydxo45@naver.com>
2022-03-10 22:57:15 +0900

    [Feat] #13 댓글 달기 기능 -기능 구현 중

--- src/components/comments/AddComments.jsx
+++ src/components/comments/AddComments.jsx
@@ -1,91 +1,108 @@
 import React from 'react';
 import Styled from 'styled-components';
 
-const AddComments = ({ pushComment }) => {
-  // 댓글 단 시간
-  let today = new Date(); // today 객체에 Date()의 결과를 넣어줬다
-  let time = {
-    year: today.getFullYear().toString().substring(2, 4), //현재 년도 앞에 두자리 자름
-    month: today.getMonth() + 1, // 현재 월
-    date: today.getDate(), // 현제 날짜
-    hours: today.getHours(), //현재 시간
-    minutes: today.getMinutes(), //현재 분
-  };
-  let timestring = `${time.year}.${time.month}.${time.date} ${time.hours}:${time.minutes}`;
+const AddComments = () => {
+  // 댓글 작성 시간 표시 함수
+  function displayedAt(createdAt) {
+    const milliSeconds = new Date() - createdAt;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  }
+
   return (
     <>
-      {/* {pushComment.map((el, idx) => { */}
-      <AddCommentsBox>
-        <TopBox>
-          <NickNameArea>test</NickNameArea>
-          <DateArea>{timestring}</DateArea>
-        </TopBox>
-        <CommentsArea>test</CommentsArea>
-      </AddCommentsBox>
-      {/* })} */}
+      <Container>
+        <AddCommentsBox>
+          <NickNameArea>yong313</NickNameArea>
+          <CommentsArea>test</CommentsArea>
+        </AddCommentsBox>
+        <ToolBox>
+          <DateArea>{displayedAt()}</DateArea>
+          <ReComment>답글달기</ReComment>
+          <ToEdit>수정하기</ToEdit>
+          <DeleteComment>삭제</DeleteComment>
+        </ToolBox>
+      </Container>
     </>
   );
 };
 
-const AddCommentsBox = Styled.div`
-  width: 90%;
+const Container = Styled.div`
+  width:100%;
   height: auto;
-  min-height: 76px;
-  margin: 17px 0;
-  border-radius: 15px;
-  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.08);
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  background-color: #efefef;
+  padding: 5px 20px 0 20px;
+`;
+
+const AddCommentsBox = Styled.div`
+  width: 100%;
+  height: auto;
+  padding: 15px 0;
   display: flex;
   align-items: flex-start;
   justify-content: center;
-  animation: fadeInMSG 0.85s cubic-bezier(0.22, 0.61, 0.36, 1);
-
-  @keyframes fadeInMSG {
-    from {
-      opacity: 0;
-      margin-left: 80px;
-    }
-    to {
-      opacity: 1;
-      margin-left: 0;
-    }
-  }
-`;
-
-const TopBox = Styled.div`
-  width: 30%;
-  height: 100%;
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  padding-top: 3px;
 `;
 
 const NickNameArea = Styled.div`
-  width: 100%;
+  width: 26%;
   height: auto;
   display: flex;
-  font-size: 1rem;
+  font-size: 1.2rem;
   font-weight: bold;
+  padding-top: 2px;
+  margin-right: 1%;
   color: #be9162;
-  margin-bottom: 7px;
-`;
-
-const DateArea = Styled.div`
-  width: 100%;
-  height: auto;
-  display: flex;
-  font-size: 11px;
-  color: #ccc;
 `;
 
 const CommentsArea = Styled.div`
-  width: 70%;
+  width: 73%;
   height: 100%;
   display: flex;
-  font-size: 0.9rem;
-  font-weight: 300;
+  font-size: 1.1rem;
   line-height: 1.425rem;
+`;
+
+const ToolBox = Styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const DateArea = Styled.div`
+  width: auto;
+  height: auto;
+  color: #999;
+  font-size: 0.85rem;
+  font-weight: 300;
+  margin-right: 15px;
+`;
+
+const ReComment = Styled(DateArea)`
+  cursor: pointer;
+`;
+
+const ToEdit = Styled(DateArea)`
+  cursor: pointer;
+`;
+
+const DeleteComment = Styled(DateArea)`
+  cursor: pointer;
+  margin-right: 0;
 `;
 
 export default AddComments;
