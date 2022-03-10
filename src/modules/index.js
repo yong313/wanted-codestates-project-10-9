a6bc273cb01d535b3109d62937046b44d18751f0
try <dlsduf@gmail.com>
2022-03-10 23:52:30 +0900

    [Merge] 머치 충돌 확인 및 더미 데이터 생성

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import comment from './comment';
-import test from './test';
+import reviews from './reviews';
 import viewQuantity from './viewQuantity';
 //! 한프로젝트에 리듀서가 여러개일경우 한 리듀서로 합쳐사용한다.
 //! 합쳐진 리듀서를 루트 리듀서라고 부른다.
 
-
-const rootReducer = combineReducers({ test, comment, viewQuantity  });
+const rootReducer = combineReducers({ viewQuantity, reviews });
 
 export default rootReducer;
 
