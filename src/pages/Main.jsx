91fca560e268ae827fb18030f033d0901c2d3ff3
try <dlsduf@gmail.com>
2022-03-11 11:12:12 +0900

    [Feat] #35 리뷰 등록 버튼 오른쪽아래에 고정
    -  리뷰등록 버튼 오른쪽 아래 고정
    -  클릭시 리뷰등록 페이지로 이동
    -  hover 이벤트효과 에니메이션 미적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,9 +5,10 @@ import CommentBox from '../components/comments/CommentBox';
 import styled from 'styled-components';
 import GridList from '../components/GridList';
 import Sort from '../components/main/Sort';
-import MoveToTopBtn from '../components/MoveToTopBtn';
+import MoveToTopBtn from '../components/main/MoveToTopBtn';
 import InfinityList from '../components/InfinityList';
 import SelectListBtn from '../components/SelectListBtn';
+import AddReviewBtn from '../components/main/AddReviewBtn';
 export default function Main() {
   const [showList, setShowList] = useState(true);
 
@@ -17,6 +18,7 @@ export default function Main() {
       <SelectListBtn showList={showList} setShowList={setShowList} />
       {showList ? <GridList /> : <InfinityList />}
       <MoveToTopBtn />
+      <AddReviewBtn />
     </Wrapper>
   );
 }
