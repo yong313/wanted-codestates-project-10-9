a93631d552c3f4dafcbae196f67c061009c80540
try <dlsduf@gmail.com>
2022-03-12 20:40:24 +0900

    [Design] #44 그리드, 리스트 배열 선택 버튼, 리스트 최상위, 리뷰생성 버튼 디자인수정 - 그리드, 리스트 선택아이콘 호버효과및 밑줄추가 - 리스트 최상위버튼 스크롤 위치에따라 보여줬다 안보여줬다하도록 구현 - 리뷰생성 버튼 애니메이션 수정

--- src/modules/viewQuantity.js
+++ src/modules/viewQuantity.js
@@ -2,16 +2,22 @@ const ADD_VIEW_QUANTITY = 'viewQuantity/ADD_VIEW_QUANTITY';
 const INIT_VIEW_QUANTITY = 'viewQuantity/INIT_VIEW_QUANTITY';
 const SET_START_INDEX = 'viewQuantity/SET_START_INDEX';
 const SET_SHOW_DETAIL_VIEW = 'viewQuantity/SET_SHOW_DETAIL_VIEW';
+const SET_SHOW_MOVE_TO_TOP_BTN = 'viewQuantity/SHOW_MOVE_TO_TOP_BTN';
 
 export const addViewQuantity = () => ({ type: ADD_VIEW_QUANTITY });
 export const initViewQuantity = () => ({ type: INIT_VIEW_QUANTITY });
 export const setStartIndex = (idx) => ({ type: SET_START_INDEX, idx });
 export const setShowDetailView = () => ({ type: SET_SHOW_DETAIL_VIEW });
+export const setShowMoveToTopBtn = (state) => ({
+  type: SET_SHOW_MOVE_TO_TOP_BTN,
+  state,
+});
 
 const initialState = {
   quantity: 29,
   startIdx: 0,
   showDetailView: false,
+  showMoveToTopBtn: false,
 };
 
 export default function viewQuantity(state = initialState, action) {
@@ -37,6 +43,11 @@ export default function viewQuantity(state = initialState, action) {
       return {
         ...state,
         showDetailView: !state.showDetailView,
+      };
+    case SET_SHOW_MOVE_TO_TOP_BTN:
+      return {
+        ...state,
+        showMoveToTopBtn: action.state,
       };
     default:
       return state;
